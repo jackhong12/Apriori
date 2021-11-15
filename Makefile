1adc9309d62d0abc29834bb15ac702c7ad808686
jackh <jackhuang1205@gmail.com>
2021-11-15 21:14:41 +0800

    add lookup table

--- Makefile
+++ Makefile
@@ -1,6 +1,17 @@
-apriori.exe : apriori.o
-	g++ -std=c++11 -o apriori.exe apriori.o
+SAN := -fsanitize=address -fsanitize=undefined -fno-sanitize-recover=all -fsanitize=float-divide-by-zero -fsanitize=float-cast-overflow -fno-sanitize=null -fno-sanitize=alignment
+CXXFLAG := -g3 -O3
+#CXXFLAG += $(SAN)
 
-apriori.o : apriori.cpp
-	g++ -std=c++11 -c -o apriori.o apriori.cpp
+apriori.exe: apriori.o
+	g++ -std=c++11 -o apriori.exe apriori.o $(CXXFLAG)
 
+apriori.o: apriori.cpp
+	g++ -std=c++11 -c -o apriori.o apriori.cpp $(CXXFLAG)
+
+vprofiling:
+	valgrind --tool=callgrind ./apriori.exe 3 ./input.txt ./output.txt
+	callgrind_annotate callgrind.out.* apriori.cpp
+
+clean:
+	rm -f apriori.o apriori.exe
+	rm -f callgrind.out.*
--- apriori.cpp
+++ apriori.cpp
@@ -6,13 +6,25 @@
 #include <fstream>
 #include <cmath>
 #include <iomanip>
+#include <algorithm>
 using namespace std;
+
+typedef vector<int> Items;
+
+struct FrequentItem {
+    Items items;
+    long double support;
+};
+
+typedef vector<Items> ItemSets;
+typedef vector<FrequentItem> KFrequentItems;
+typedef vector<KFrequentItems> FrequentTable;
 
 long double round(long double value, int pos){
     long double temp;
-    temp = value * pow( 10, pos );
-    temp = floor( temp + 0.5 );
-    temp *= pow( 10, -pos );
+    temp = value * pow(10, pos);
+    temp = floor(temp + 0.5);
+    temp *= pow(10, -pos);
     return temp;
 }
 
@@ -20,99 +32,125 @@ class Apriori {
 private:
     int nowStep;
     long double minSupport;
-    vector<vector<int> > transactions;
-    vector<vector<int> > C, L;
-    vector<vector<vector<int> > > frequentSet;
-    vector<tuple<vector<int>, vector<int>, long double, long double> > associationRules;
+    ItemSets transactions;
+    ItemSets kItemSets;
+    KFrequentItems kFrequentItems;
+    FrequentTable frequentTable;
+    vector<tuple<vector<int>, vector<int>, long double, long double>> associationRules;
 public:
-    Apriori (vector<vector<int> > _transactions, long double _minSupport) {
-        nowStep=0;
+    Apriori (ItemSets _transactions, long double _minSupport) {
+        nowStep = 0;
         minSupport = _minSupport;
-        for(auto&row:_transactions){
+        for(Items &row: _transactions){
             sort(row.begin(), row.end());
             transactions.push_back(row);
         }
-        frequentSet.push_back({{}});
     }
-    
+
     vector<tuple<vector<int>, vector<int>, long double, long double> > getAssociationRules(){
         return associationRules;
     }
-    
+
     void process() {
         while(true) {
-            C = generateNextC();
-            if(C.size()==0) break;
             nowStep++;
-            
-            L = generateL();
-            frequentSet.push_back(L);
+            // step 1
+            kItemSets = generateNextKItems();
+            if (kItemSets.size() == 0) break;
+
+            // step 2
+            KFrequentItems frequentItems = generateKFrequentItems();
+
+            // step 3
+            kFrequentItems.clear();
+            for (FrequentItem fi: frequentItems)
+                if (round(fi.support, 2) > minSupport)
+                    kFrequentItems.push_back(fi);
+            frequentTable.push_back(kFrequentItems);
         }
-        
-        for(auto&stepItemSet:frequentSet) {
-            for(auto&items:stepItemSet) {
-                generateAssociationRule(items, {}, {}, 0);
+
+        for(KFrequentItems &kfItems: frequentTable) {
+            for(FrequentItem &f: kfItems) {
+                generateAssociationRule(f.items, {}, {}, 0);
             }
         }
     }
-    
-    void generateAssociationRule(vector<int> items, vector<int> X, vector<int> Y, int index) {
+
+    long double lookupFrequentTable (Items items) {
+        int k = items.size();
+        for (FrequentItem &fi: frequentTable[k - 1]) {
+            int i;
+            for (i = 0; i < k; i++) {
+                if (fi.items[i] != items[i])
+                    break;
+            }
+            if (i == k)
+                return fi.support;
+        }
+        return 0;
+    }
+
+    void generateAssociationRule (Items items, Items X, Items Y, int index) {
         if(index == items.size()) {
             if(X.size()==0 || Y.size() == 0) return;
-            long double XYsupport = getSupport(items);
-            long double Xsupport = getSupport(X);
-            
+            long double XYsupport = lookupFrequentTable(items);
+            long double Xsupport = lookupFrequentTable(X);
+
             if(Xsupport == 0) return;
-            
+
             long double support = (long double)XYsupport;
-            long double confidence = (long double)XYsupport/Xsupport*100.0;
+            long double confidence = (long double)XYsupport / Xsupport * 100.0;
             associationRules.push_back({X, Y, support, confidence});
             return;
         }
-        
+
         X.push_back(items[index]);
         generateAssociationRule(items, X, Y, index+1);
         X.pop_back();
         Y.push_back(items[index]);
         generateAssociationRule(items, X, Y, index+1);
     }
-    
-    vector<int> getElement(vector<vector<int> > itemset) {
-        vector<int> element;
+
+    Items getElement (ItemSets itemset) {
+        Items element;
         set<int> s;
-        for(auto&row:itemset) for(auto&col:row) s.insert(col);
-        for(auto iter=s.begin(); iter != s.end(); iter++) element.push_back(*iter);
+        for (Items &row: itemset)
+            for (int &col: row) s.insert(col);
+        for (auto iter=s.begin(); iter != s.end(); iter++)
+            element.push_back(*iter);
         return element;
     }
-    
-    vector<vector<int> > generateNextC() {
-        if(nowStep==0) {
-            vector<vector<int> > ret;
-            vector<int> element = getElement(transactions);
-            for(auto&i:element) ret.push_back(vector<int>(1, i));
-            
+
+    ItemSets generateNextKItems () {
+        if(nowStep == 1) {
+            ItemSets ret;
+            Items element = getElement(transactions);
+            for(auto &i: element)
+                ret.push_back(vector<int>(1, i));
             return ret;
         } else {
             return pruning(joining());
         }
     }
-    
-    vector<vector<int> > joining () {
-        vector<vector<int> > ret;
-        for(int i=0;i<L.size();i++){
-            for(int j=i+1;j<L.size(); j++) {
+
+    ItemSets joining () {
+        ItemSets ret;
+        for(int i = 0; i < kFrequentItems.size(); i++){
+            for(int j = i + 1; j < kFrequentItems.size(); j++) {
+                Items &f1 = kFrequentItems[i].items;
+                Items &f2 = kFrequentItems[j].items;
                 int k;
-                for(k=0;k<nowStep-1; k++) {
-                    if(L[i][k] != L[j][k]) break;
+                for(k = 0; k < nowStep - 2; k++) {
+                    if(f1[k] != f2[k]) break;
                 }
-                if(k == nowStep-1) {
-                    vector<int> tmp;
-                    for(int k=0;k<nowStep-1; k++) {
-                        tmp.push_back(L[i][k]);
+                if(k == nowStep - 2) {
+                    Items tmp;
+                    for(int k=0; k < nowStep - 2; k++) {
+                        tmp.push_back(f1[k]);
                     }
-                    int a = L[i][nowStep-1];
-                    int b = L[j][nowStep-1];
-                    if(a>b) swap(a,b);
+                    int a = f1[nowStep - 2];
+                    int b = f2[nowStep - 2];
+                    if(a > b) swap(a, b);
                     tmp.push_back(a), tmp.push_back(b);
                     ret.push_back(tmp);
                 }
@@ -120,51 +158,52 @@ public:
         }
         return ret;
     }
-    
-    vector<vector<int> > pruning (vector<vector<int> > joined) {
-        vector<vector<int> > ret;
-        
-        set<vector<int> > lSet;
-        for(auto&row:L) lSet.insert(row);
-        
-        for(auto&row:joined){
+
+    ItemSets pruning (ItemSets joined) {
+        ItemSets ret;
+
+        set<Items> lSet;
+        for(FrequentItem &row: kFrequentItems) lSet.insert(row.items);
+
+        for(Items &row: joined){
             int i;
-            for(i=0;i<row.size();i++){
-                vector<int> tmp = row;
-                tmp.erase(tmp.begin()+i);
+            for(i = 0; i < row.size(); i++) {
+                Items tmp = row;
+                tmp.erase(tmp.begin() + i);
                 if(lSet.find(tmp) == lSet.end()) {
                     break;
                 }
             }
-            if(i==row.size()){
+            if(i == row.size()){
                 ret.push_back(row);
             }
         }
         return ret;
     }
-    
-    long double getSupport(vector<int> item) {
+
+    long double getSupport (Items item) {
         int ret = 0;
-        for(auto&row:transactions){
+        // TODO: parallize
+        for(auto &row: transactions){
             int i, j;
             if(row.size() < item.size()) continue;
-            for(i=0, j=0; i < row.size();i++) {
-                if(j==item.size()) break;
+            for(i=0, j=0; i < row.size(); i++) {
+                // throttle
+                if(j == item.size()) break;
                 if(row[i] == item[j]) j++;
             }
-            if(j==item.size()){
+            if(j == item.size()){
                 ret++;
             }
         }
-        return (long double)ret/transactions.size()*100.0;
+        return (long double)ret / transactions.size() * 100.0;
     }
-    
-    vector<vector<int> > generateL() {
-        vector<vector<int> > ret;
-        for(auto&row:C){
+
+    KFrequentItems generateKFrequentItems () {
+        KFrequentItems ret;
+        for(auto &row: kItemSets){
             long double support = getSupport(row);
-            if(round(support, 2) < minSupport) continue;
-            ret.push_back(row);
+            ret.push_back({row, support});
         }
         return ret;
     }
@@ -173,9 +212,9 @@ public:
 class InputReader {
 private:
     ifstream fin;
-    vector<vector<int> > transactions;
+    ItemSets transactions;
 public:
-    InputReader(string filename) {
+    InputReader (string filename) {
         fin.open(filename);
         if(!fin) {
             cout << "Input file could not be opened\n";
@@ -185,10 +224,10 @@ public:
     }
     void parse() {
         string str;
-        while(!getline(fin, str).eof()){
+        while (!getline(fin, str).eof()) {
             vector<int> arr;
             int pre = 0;
-            for(int i=0;i<str.size();i++){
+            for (int i = 0; i < str.size(); i++){
                 if(str[i] == '\t') {
                     int num = atoi(str.substr(pre, i).c_str());
                     arr.push_back(num);
@@ -197,11 +236,11 @@ public:
             }
             int num = atoi(str.substr(pre, str.size()).c_str());
             arr.push_back(num);
-            
+
             transactions.push_back(arr);
         }
     }
-    vector<vector<int> > getTransactions() {
+    ItemSets getTransactions() {
         return transactions;
     }
 };
@@ -220,24 +259,24 @@ public:
         associationRules = _associationRules;
         buildOutput();
     }
-    
+
     void buildOutput() {
         for(auto&i:associationRules) {
             fout << vectorToString(get<0>(i)) << '\t';
             fout << vectorToString(get<1>(i)) << '\t';
-            
+
             fout << fixed;
             fout.precision(2);
             fout << get<2>(i) << '\t';
-            
+
             fout << fixed;
             fout.precision(2);
             fout << get<3>(i);
-            
+
             fout << endl;
         }
     }
-    
+
     string vectorToString(vector<int> arr) {
         string ret = "{";
         for(int i=0;i<arr.size();i++){
@@ -258,7 +297,7 @@ public:
     Checker(string filename1, string filename2) {
         fin1.open(filename1);
         fin2.open(filename2);
-        
+
         if(!fin1 || !fin2) {
             cout << "Input file could not be opened\n";
             exit(0);
@@ -266,7 +305,7 @@ public:
     }
     void compare() {
         file1ToSet();
-        
+
         string str;
         while(!getline(fin2, str).eof()){
             str.pop_back();
@@ -292,33 +331,33 @@ int main (int argc, char ** argv) {
     string minSupport(argv[1]);
     string inputFileName(argv[2]);
     string outputFileName(argv[3]);
-    
+
     /*
-    vector<vector<int> > transactions = {
-        {1, 2, 5},
-        {2,4},
-        {2,3},
-        {1, 2, 4},
-        {1, 3},
-        {2, 3},
-        {1, 3},
-        {1, 2, 3, 5},
-        {1, 2, 3}
-    };
-    */
-    
+       vector<vector<int> > transactions = {
+       {1, 2, 5},
+       {2,4},
+       {2,3},
+       {1, 2, 4},
+       {1, 3},
+       {2, 3},
+       {1, 3},
+       {1, 2, 3, 5},
+       {1, 2, 3}
+       };
+       */
+
     InputReader inputReader(inputFileName);
-    vector<vector<int> > transactions = inputReader.getTransactions();
+    ItemSets transactions = inputReader.getTransactions();
     Apriori apriori(transactions, stold(minSupport));
     apriori.process();
     OutputPrinter outputPrinter(outputFileName, apriori.getAssociationRules());
-    
-    /*
-    for test
-    Checker checker("output5.txt", "outputRsupport5.txt");
-    checker.compare();
-    */
 
-    
+    /*
+       for test
+       Checker checker("output5.txt", "outputRsupport5.txt");
+       checker.compare();
+       */
+
+
     return 0;
 }
